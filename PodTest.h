e8b6d9db1e28449eb97d35c062c01925b61e2ebc
Jashion <ycyz138258886@qq.com>
2017-11-25 13:10:57 +0800

    PodTest

---
+++ PodTest.h
@@ -0,0 +1,15 @@
+//
+//  PodTest.h
+//  TestOther
+//
+//  Created by Jashion on 2017/11/25.
+//  Copyright © 2017年 BMu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PodTest : NSObject
+
++ (void)log;
+
+@end
---
+++ PodTest.m
@@ -0,0 +1,17 @@
+//
+//  PodTest.m
+//  TestOther
+//
+//  Created by Jashion on 2017/11/25.
+//  Copyright © 2017年 BMu. All rights reserved.
+//
+
+#import "PodTest.h"
+
+@implementation PodTest
+
++ (void)log {
+    NSLog(@"%@", [NSDate new]);
+}
+
+@end
